4fb801474d5f346766ff797ec8a7fa78d62aec3a
Harshita Hiremath <59929466+harshita250@users.noreply.github.com>
2022-04-18 00:50:12 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -464,7 +464,7 @@ dataset = pd.read_csv("dataset_phishing.csv")
 data = pd.read_csv("phishing.csv")
 index = data.shape[0] 
 step=0
-for step in range(0,10):
+for step in range(0,3):
     url=dataset['url'][step]
     row = generate_data_set(url,index)
     index = index + 1
