6de4d56d63d79247579fad71da3c99eacadf02be
Harshita Hiremath <59929466+harshita250@users.noreply.github.com>
2022-04-17 23:13:42 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -453,7 +453,7 @@ def generate_data_set(url,index):
 
     return data_set
 
-dataset = pd.read_csv("dataset_phishing.csv")
+'''dataset = pd.read_csv("dataset_phishing.csv")
 data = pd.read_csv("phishing.csv")
 index = data.shape[0] 
 step=0
@@ -465,4 +465,4 @@ for step in range(0,10):
     # create the csv writer
       writer = csv.writer(f)
     # write a row to the csv 
-      writer.writerow(row)
+      writer.writerow(row)'''
