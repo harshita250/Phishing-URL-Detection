20fea740c838c8b0c690a374c335e2b2f5ab98d2
Harshita Hiremath <59929466+harshita250@users.noreply.github.com>
2022-04-17 23:48:49 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -19,7 +19,6 @@ def diff_month(d1, d2):
 
 
 def clean_dataset(df):
-    assert isinstance(df, pd.DataFrame), "df needs to be a pd.DataFrame"
     df.dropna(inplace=True)
     indices_to_keep = ~df.isin([np.nan, np.inf, -np.inf]).any(1)
     return df[indices_to_keep].astype(np.float64)
